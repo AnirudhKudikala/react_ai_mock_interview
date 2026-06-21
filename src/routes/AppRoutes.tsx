e7d2b419af76517c31982b5147259dd073d9bb59
AnirudhKudikala <anirudhkudikala01@gmail.com>
2026-06-21 18:47:48 +0530

    Clerk changes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -9,7 +9,7 @@ function AppRoutes() {
 
   return (
     <BrowserRouter>
-        <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY} signInUrl="/sign-in">
+        <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY} signInUrl="/sign-in" signUpUrl="/sign-up">
             <Routes>
                 {routesConfig.map((route) => (
                 <Route
